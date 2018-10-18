4e533394f43e11443c86d32b40479964954f9a1b
GarciaDFE <garcia.dfe@gmail.com>
2018-10-18 18:13:47 -0300

    Início treino Gulp aplicando otimizações no site Shiro 2

---
+++ src/js/components/main-btnMenu.js
@@ -0,0 +1,19 @@
+const $menu = document.querySelector(".menu");
+const $btnOpenMenu = document.querySelector(".btnMenuOpen");
+const $btnCloseMenu = document.querySelector(".btnMenuClose");
+
+function clickBtnOpenMenu() {
+	$menu.classList.add("menu-open");
+	$btnOpenMenu.classList.remove("imgBtnOpen");
+
+	event.preventDefault();
+}
+
+function clickBtnCloseMenu() {
+    $menu.classList.remove("menu-open");
+	$btnOpenMenu.classList.add("imgBtnOpen");
+  	event.preventDefault();
+}
+
+$btnOpenMenu.addEventListener("click", clickBtnOpenMenu);
+$btnCloseMenu.addEventListener("click", clickBtnCloseMenu);
